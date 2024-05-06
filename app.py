0f39e1c6f8e61378008a209d69331bd1c60f3272
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2024-05-06 18:03:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import os
+import spacy
+
+import cv2
+import os,argparse
+import pytesseract
+from PIL import Image
+
+
+
+import warnings
+warnings.filterwarnings("ignore")
+st.header("Name Identification in text", anchor=None)
+
+def contains_name(text):
+    """
+    nlp = spacy.load('en_core_web_lg')
+    doc = nlp(text)
+    names = []
+    for ent in doc.ents:
+        if ent.label_ == 'PERSON':
+            names.append(ent.text)
+    """
+    return 0
+
+text = st.text_input('Enter Text',label_visibility="visible")
+if st.button("Submit"):
+    names_idn = contains_name(text)
+    if len(names_idn) == 0:
+        st.error('No names identified in the text')
+    else:
+        st.success('Below names identified in the text')
+        st.write(names_idn)
+
+        
+  
+    
