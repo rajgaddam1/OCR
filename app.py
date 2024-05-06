ca54208566ab474632909a63dc1adbdf92bdb6c7
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2024-05-06 18:53:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ warnings.filterwarnings("ignore")
 st.header("OCR Model Extract text from images", anchor=None)
 
 def text_extraction_from_image(image_uploaded):
-    image = cv2.imread(image_uploaded, 0)
+    image = cv2.imread(image_uploaded)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     extract_text = pytesseract.image_to_string(image)
@@ -19,9 +19,8 @@ def text_extraction_from_image(image_uploaded):
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-    extract_text = text_extraction_from_image(uploaded_file)
+    image_name = uploaded_file.name
+    extract_text = text_extraction_from_image(image_name)
     st.write(extract_text)
 
 
