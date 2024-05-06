1d76a0578de2c74806de9b4877d5255337db2138
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2024-05-06 19:37:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def text_extraction_from_image(image_uploaded):
 
 uploaded_file = st.file_uploader("Choose a file", type=["jpg", "png", "jpeg"])
 st.button("Extract Text", type="primary")
-if st.button("Say hello"):
+if st.button():
     if uploaded_file is not None:
         extract_text = text_extraction_from_image(uploaded_file)
         st.write(extract_text)
